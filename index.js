4fdfb72336f9d402e82966bc3e9fc6eeb5fe7071
Daniel Reardon <danielreardon@google.com>
2025-06-05 13:11:37 +0000

    Address issue where Setting query_expansion_spec proto fields is not allowed for multi-datastore search

--- index.js
+++ index.js
@@ -201,7 +201,7 @@ app.post('/auth/azure/callback',
                             'Authorization': `Bearer ${googleToken}`,
                             'Content-Type': 'application/json'
                         },
-                        body: JSON.stringify({"query":searchQuery,"pageSize":10,"queryExpansionSpec":{"condition":"AUTO"},"spellCorrectionSpec":{"mode":"AUTO"},"relevanceScoreSpec":{"returnRelevanceScore":true},"languageCode":"en-US","naturalLanguageQueryUnderstandingSpec":{"filterExtractionCondition":"ENABLED"},"userInfo":{"timeZone":"America/New_York"}})
+                        body: JSON.stringify({"query":searchQuery,"pageSize":10,"spellCorrectionSpec":{"mode":"AUTO"},"relevanceScoreSpec":{"returnRelevanceScore":true},"languageCode":"en-US","naturalLanguageQueryUnderstandingSpec":{"filterExtractionCondition":"ENABLED"},"userInfo":{"timeZone":"America/New_York"}})
                     });
 
                     const searchResults = await discoveryResponse.json();
